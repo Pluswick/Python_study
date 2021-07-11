d7abd1e3b3139ddd0a5985b35ec0c0197c70f746
DESKTOP-3GOTUVS\tjdgk <tjdgks0814@gmail.com>
2021-07-11 16:00:09 +0900

    Assignment(2).py
    Group_study assignment. Basics of Python grammers

---
+++ Assignment(2).py
@@ -0,0 +1,31 @@
+'''
+#list join
+
+a=['Life', 'is', 'too', 'short']
+result=" ".join(a)
+print(result)
+'''
+'''
+#tuple add
+
+a=(1, 2, 3)
+a= a+(4,)
+print(a)
+'''
+'''
+#dictioinary pop
+
+a={'A':90, 'B':80, 'C':70}
+pop=a.pop('B')
+print(a)
+print(pop)
+'''
+'''
+#delete duplicated numbers in list
+a=[1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,4,4,5,5,5,5,5,5,]
+print(a)
+aSet=set(a)
+print(aSet)
+b=list(aSet)
+print(b)
+'''
